e3d51d0c11cf3b3b8c037539efadec084d30ef24
clemence <clemencegoh@gmail.com>
2024-11-30 11:32:57 +0800

    add in some music

--- src/world/hud.gd
+++ src/world/hud.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 @onready var actualScoreLabel: Label
+@export var gameoverSound: AudioStreamPlayer3D
+@export var blocksFreeSound: AudioStreamPlayer3D
+@export var bgm: AudioStreamPlayer3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,12 +13,13 @@ func _ready() -> void:
 	
 func change_score():
 	actualScoreLabel.text = str(GameManager.current_score)
+	blocksFreeSound.play()
 
 func show_gameover():
+	gameoverSound.play()
 	get_tree().paused = true
 	$"./Gameover".visible = true
 	$"./ResetButton".visible = true
-
 
 func _on_button_pressed() -> void:
 	get_tree().paused = false
